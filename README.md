c0067461e25acddade50771df6f0dd0ecdde3896
profesorfalken <jgarciaalo@gmail.com>
2015-12-16 17:03:14 +0100

    Creation of v0.1

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# PayzenWebServicesSDK
-Java SDK for Payzen SOAP Web Services v5
+# Payzen SDK SOAP Web Services v5 in Java #
+
+This SDK simplifies the use of Web Services SOAP Payzen v5 in order to make payment operations. 
+
+## Basic Usage ##
+To use this SDK in your Java project you can download the last version here. 
+
+The only dependencies are:
+
+- *commons-codec-1.10.jar*
+- *commons-lang-2.6.jar*
+- *slf4j-api-1.7.12.jar*
+
+Once downloaded all the JAR files and placed in the classpath of your application, it is necessary to set up the config file.
+
+In order to do that you have to create a resource file called *payzen-config.properties* into your classpath and set the following configuration params:
+
+    shopId=[shop identifier]
+    shopKey=[shop private key]
+    mode=[mode TEST or PROD]
+    endpointHost=[the name of the host. Ex: secure.payzen.eu]
+
+
+Once the configuration is set up, use the web services is as simple as doing: 
+
+    create("Test Order",
+                100, 
+                978,
+                "4970100000000003",
+                12,
+                2017,
+                "123",
+                (result) -> "Payment Done"
+    );
+ 
+Really, is that easy.
+
+
+
+## More examples ##
+
+**Create payment using builder objects:** 
+
+    OrderRequestBuilder orderRequestBuilder
+        = OrderRequestBuilder
+        .create()
+        .orderId("TestTRS");
+
+    PaymentRequestBuilder paymentRequestBuilder
+        = PaymentRequestBuilder
+        .create()
+        .amount(100)
+        .currency(978);
+
+    CardRequestBuilder cardRequestBuilder
+        = CardRequestBuilder
+        .create()
+        .number("4970100000000003")
+        .scheme("CB")
+        .expiryMonth(12)
+        .expiryYear(2017)
+        .cardSecurityCode("123");
+
+    create(PaymentBuilder
+        .getBuilder()
+        .order(orderRequestBuilder.build())
+        .payment(paymentRequestBuilder.build())
+        .card(cardRequestBuilder.build())
+        .comment("Test payment")
+        .buildCreate(),
+        (result) -> "Payment Done"
+    );
+
+
+**Log all the response:**
+
+    create("Test Order",
+                100, 
+                978,
+                "4970100000000003",
+                12,
+                2017,
+                "123",
+                new LogResponseHandler()
+    );
+
+
+**Use annonymous functions as callback handlers (needed for Java < 8)**
+
+
+    create("Test Order",
+                100, 
+                978,
+                "4970100000000003",
+                12,
+                2017,
+                "123",
+                new ResponseHandler() {
+		            @Override
+		            public void handle(ServiceResult result) throws Exception {
+		                //Process response here
+		            }
+		        });
+    );
+
+
+**Handle the response in a classic way, not using callbacks**
+
+    ServiceResult result = create("Test Order",
+                100, 
+                978,
+                "4970100000000003",
+                12,
+                2017,
+                "123"
+    );
+
+    //Process payment result using getters
+
+## How to build the library ##
+
+
+- Install maven
+
+
+- Download and import the project into your IDE or simply clone it from Git.
+
+- First time run from root directory: *mvn -Pgenerate-ws-stubs install*. This will generate all the web service stubs.
+
+- From that time, it is enough to execute *mvn install* to regenerate the library.
+
+- If the tests fail because the configuration data is not right you can skip the tests using *mvn install -Dmaven.test.skip=true*
+
---
+++ src/test/java/org/pfalken/payzen/webservices/sdk/PaymentTest.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright 2015 Javier Garcia Alonso.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pfalken.payzen.webservices.sdk;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.pfalken.payzen.webservices.sdk.Payment.*;
+import org.pfalken.payzen.webservices.sdk.builder.PaymentBuilder;
+import org.pfalken.payzen.webservices.sdk.builder.request.CardRequestBuilder;
+import org.pfalken.payzen.webservices.sdk.builder.request.OrderRequestBuilder;
+import org.pfalken.payzen.webservices.sdk.builder.request.PaymentRequestBuilder;
+import org.pfalken.payzen.webservices.sdk.handler.response.LogResponseHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author javier
+ */
+public class PaymentTest {
+    private static final Logger logger = LoggerFactory.getLogger(LogResponseHandler.class);
+
+    public PaymentTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class Payment.
+     */
+    @Test
+    public void testCreate() {
+        logger.info("Test create simple");
+
+        ServiceResult result = create("TestTRS",
+                100,
+                978,
+                "4970100000000003",
+                12,
+                2017,
+                "123",
+                new LogAndCheckResponseHandler()
+        );
+
+        String trsUuid = (result != null && result.getPaymentResponse() != null && result.getPaymentResponse().getTransactionUuid() != null)
+                ? result.getPaymentResponse().getTransactionUuid() : null;
+
+        Assert.assertNotNull("Not UUID generated", trsUuid);
+
+        //Verify transaction
+        details(trsUuid, new LogAndCheckResponseHandler());
+
+        logger.info("End Test create");
+    }
+
+    /**
+     * Test of create method, of class Payment.
+     */
+    @Test
+    public void testCreateFull() {
+        logger.info("Test create full");
+
+        String trsUuid = createNormalPayment("4970100000000003");
+
+        Assert.assertNotNull("Not UUID generated", trsUuid != null);
+
+        //Verify transaction
+        details(trsUuid, new LogAndCheckResponseHandler());
+
+        logger.info("End create full");
+    }
+
+    /**
+     * Test of details method, of class Payment.
+     */
+    @Test
+    public void testDetails() {
+        logger.info("Test details simple");
+
+        String trsUuid = createNormalPayment("4970100000000003");
+
+        Assert.assertNotNull("Not UUID generated", trsUuid != null);
+
+        ServiceResult result = details(trsUuid, new LogAndCheckResponseHandler());
+        int responseCode = result.getCommonResponse().getResponseCode();
+        Assert.assertTrue("Cannot get details. Error code: " + responseCode, responseCode == 0);
+
+        logger.info("Test details full");
+    }
+
+    /**
+     * Test of details method, of class Payment.
+     */
+    @Test
+    public void testDetailsByFind() {
+        logger.info("Test details using find");
+
+        OrderRequestBuilder orderRequestBuilder
+                = OrderRequestBuilder
+                .create()
+                .orderId("TestTRS");
+
+        PaymentRequestBuilder paymentRequestBuilder
+                = PaymentRequestBuilder
+                .create()
+                .amount(100)
+                .currency(978);
+
+        CardRequestBuilder cardRequestBuilder
+                = CardRequestBuilder
+                .create()
+                .number("4970100000000003")
+                .scheme("CB")
+                .expiryMonth(12)
+                .expiryYear(2017)
+                .cardSecurityCode("123");
+
+        ServiceResult result = create(PaymentBuilder
+                .getBuilder()
+                .order(orderRequestBuilder.build())
+                .payment(paymentRequestBuilder.build())
+                .card(cardRequestBuilder.build())
+                .comment("Test payment")
+                .buildCreate(),
+                new LogAndCheckResponseHandler()
+        );
+        Assert.assertNotNull("Not UUID generated", result.getPaymentResponse());
+        Assert.assertNotNull("Not UUID generated", result.getPaymentResponse().getTransactionId());
+        Assert.assertNotNull("Not UUID generated", result.getPaymentResponse().getSequenceNumber());
+        Assert.assertNotNull("Not UUID generated", result.getPaymentResponse().getCreationDate());
+
+        String transactionId = result.getPaymentResponse().getTransactionId();
+        int sequenceNumber = result.getPaymentResponse().getSequenceNumber();
+        Date creationDate = result.getPaymentResponse().getCreationDate().toGregorianCalendar().getTime();
+
+        result = details(transactionId, creationDate, sequenceNumber, new LogAndCheckResponseHandler());
+
+        int responseCode = result.getCommonResponse().getResponseCode();
+        Assert.assertTrue("Cannot get details. Error code: " + responseCode, responseCode == 0);
+
+        logger.info("Test details full");
+    }
+
+    private String createNormalPayment(String cardNumber) {
+        String trsUuid = null;
+
+        OrderRequestBuilder orderRequestBuilder
+                = OrderRequestBuilder
+                .create()
+                .orderId("TestTRS");
+
+        PaymentRequestBuilder paymentRequestBuilder
+                = PaymentRequestBuilder
+                .create()
+                .amount(100)
+                .currency(978);
+
+        CardRequestBuilder cardRequestBuilder
+                = CardRequestBuilder
+                .create()
+                .number(cardNumber)
+                .scheme("CB")
+                .expiryMonth(12)
+                .expiryYear(2017)
+                .cardSecurityCode("123");
+
+        ServiceResult result = create(PaymentBuilder
+                .getBuilder()
+                .order(orderRequestBuilder.build())
+                .payment(paymentRequestBuilder.build())
+                .card(cardRequestBuilder.build())
+                .comment("Test payment")
+                .buildCreate(),
+                new LogAndCheckResponseHandler()
+        );
+
+        if (result != null && result.getPaymentResponse() != null) {
+            trsUuid = result.getPaymentResponse().getTransactionUuid();
+        }
+
+        return trsUuid;
+    }
+    
+    private class LogAndCheckResponseHandler extends LogResponseHandler {
+
+        @Override
+        public void handle(ServiceResult response) throws Exception {
+            super.handle(response);
+
+            //Add checks here
+            int errorCode = response.getCommonResponse().getResponseCode();
+            Assert.assertTrue("The web service returned an error code: " + errorCode, errorCode == 0);
+        }
+    }
+
+}
